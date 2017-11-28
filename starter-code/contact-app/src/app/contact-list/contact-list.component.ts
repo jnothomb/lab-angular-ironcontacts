12ad40db25fadbf2b2e71d92e1b6810577df4f13
Jean Nothomb <jeannothomb@gmail.com>
2017-11-28 15:35:15 +0100

    pushing complete pair programming exercise (no pull request) - working contacts list

--- starter-code/contact-app/src/app/contact-list/contact-list.component.ts
+++ starter-code/contact-app/src/app/contact-list/contact-list.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import contactList from '../contacts';
+import { Component, OnInit } from "@angular/core";
+import contactList from "../contacts";
 
 @Component({
-  selector: 'app-contact-list',
-  templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  selector: "app-contact-list",
+  templateUrl: "./contact-list.component.html",
+  styleUrls: ["./contact-list.component.css"]
 })
 export class ContactListComponent implements OnInit {
   contacts: Object[];
-  newContact: Object = {};
+  newContact = {
+    name: "",
+    email: "",
+    phoneNumber: "",
+    image: ""
+  };
 
   constructor() { }
 
@@ -16,9 +21,22 @@ export class ContactListComponent implements OnInit {
     this.contacts = contactList;
   }
 
-  addContact(){
+  addContact() {
     console.log("Add contact has been called");
     // add contact to contacts list
+    this.contacts.push({
+      name: this.newContact.name,
+      email: this.newContact.email,
+      phoneNumber: this.newContact.phoneNumber,
+      image: this.newContact.image
+    });
+
     // clear inputs
+    this.newContact = {
+      name: "",
+      email: "",
+      phoneNumber: "",
+      image: ""
+    };
   }
 }
